6090a5ad6556098e366aca2a5922633d4ef60f98
Salah Abdullahi <fawzar@Salahs-MBP.ahcguestnet.com>
2020-05-18 18:23:17 -0500

    updated front end and backend

--- booksapp/client/src/App.js
+++ booksapp/client/src/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import Nav from './components/Nav';
-import Books from './pages/Books';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Nav from "./components/Nav";
+import { Container} from "./components/Grid";
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path = {["/", "/books"]}>
-              <Books/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-
+      <div>
+        <Nav/>
+        <Router>
+          <Container>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            transition={Zoom}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable={false}
+            pauseOnHover
+          />
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route path="/bookshelf" component={Saved} />
+              <Route exact path="/books/:id" component={null} />
+              <Route component={null} />
+            </Switch>
+          </Container>
+        </Router>
+      </div>
     );
   }
-
 }
 
 export default App;
