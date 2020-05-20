d05a238831a89bba28926f08b156e96f5c84acda
Salah Abdullahi <fawzar@Salahs-MBP.ahcguestnet.com>
2020-05-19 21:43:56 -0500

    changed the search page

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -65,17 +65,18 @@ class Search extends Component {
               synopsis={book.volumeInfo.description ? 
                 book.volumeInfo.description : "No Description Available"}
               link={book.volumeInfo.infoLink}
-              thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+              thumbnail={ book.volumeInfo && book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? 
                 book.volumeInfo.imageLinks.thumbnail : "#"}
               />
 
               <AddBookBtn
+              
               authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
               title={book.volumeInfo.title}
               synopsis={book.volumeInfo.description ? 
                 book.volumeInfo.description : "No Description Available"}
               link={book.volumeInfo.infoLink}
-              thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+              thumbnail={ book.volumeInfo && book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? 
                 book.volumeInfo.imageLinks.thumbnail : "#"}
               
               />
